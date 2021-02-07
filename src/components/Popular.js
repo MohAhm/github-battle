4cde6571add69675830d58dc156e7f5406ec2392
Mohamed Ali <a.mohamed.cali@gmail.com>
2021-02-07 22:22:45 +0100

    Fetching and caching repos

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react'
+import { fetchPopularRepos } from '../utils/api'
 import LanguagesNav from './LanguagesNav'
 
 export default class Popular extends Component {
 	state = {
-		selectedLanguage: 'All'
+		selectedLanguage: 'All',
+		repos: {},
+		error: null
+	}
+
+	componentDidMount () {
+		this.updateLanguage(this.state.selectedLanguage)
 	}
 
 	updateLanguage = (selectedLanguage) => {
 		this.setState({
-			selectedLanguage
+			selectedLanguage,
+			error: null
 		})
+
+		if (!this.state.repos[selectedLanguage]) {
+			fetchPopularRepos(selectedLanguage)
+				.then(data => {
+					this.setState(({ repos }) => ({
+						repos: {
+							...repos,
+							[selectedLanguage]: data
+						}
+					}))
+				})
+				.catch(error => {
+					console.log('Error fetching repos: ', error)
+
+					this.setState({
+						error: `There was an error fetching the repositories.`
+					})
+				})
+		}
+	}
+
+	isLoading = () => {
+		const { selectedLanguage, repos, error } = this.state
+
+		return !repos[selectedLanguage] && error === null
 	}
 
 	render() {
-		const { selectedLanguage } = this.state
+		const { selectedLanguage, repos, error } = this.state
 
 		return (
 			<>
@@ -21,6 +54,16 @@ export default class Popular extends Component {
 					selected={selectedLanguage}
 					onUpdateLanguage={this.updateLanguage}
 				/>
+
+				{this.isLoading() && <p>LOADING</p>}
+
+				{error && <p>{error}</p>}
+
+				{repos[selectedLanguage] &&
+					<pre>
+						{JSON.stringify(repos[selectedLanguage], null, 2)}
+					</pre>
+				}
 			</>
 		)
 	}
