32c146302d002ca7308e1f051938259f03b86825
Mohamed Ali <a.mohamed.cali@gmail.com>
2021-02-13 22:23:48 +0100

    Render player input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Battle from './components/Battle'
 import Popular from './components/Popular'
 
 class App extends React.Component {
 	render () {
 		return (
 			<div className="container">
-				<Popular />
+				{/* <Popular /> */}
+				<Battle />
 			</div>
 		)
 	}
