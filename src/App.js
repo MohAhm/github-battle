b314a724434cadbf026ab7e72464e634d96a5db8
Mohamed Ali <a.mohamed.cali@gmail.com>
2021-04-19 19:34:43 +0200

    Bypassing Props with Context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import Battle from './components/Battle'
+import Nav from './components/Nav'
 import Popular from './components/Popular'
+import { ThemeProvider } from './contexts/theme'
 
 class App extends React.Component {
+	state = {
+		them: 'light',	
+		toggleTheme: () => {
+			this.setState(({ theme }) => ({
+				theme: theme === 'light' ? 'dark' : 'light'
+			}))
+		}
+	}
+
 	render () {
 		return (
-			<div className="container">
-				{/* <Popular /> */}
-				<Battle />
-			</div>
+			<ThemeProvider value={this.state}>
+				<div className={this.state.theme}>
+					<div className="container">
+						<Nav />
+						<Popular />
+						{/* <Battle /> */}
+					</div>
+				</div>
+			</ThemeProvider>
 		)
 	}
 }
